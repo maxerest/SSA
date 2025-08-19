7b2396bceab0d90257534ff7cfe306d733be523d
maxerest <62623274+maxerest@users.noreply.github.com>
2025-08-19 22:37:46 +0200

    Update App.java
    Change 3 to create VS clone

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is after my 2nd test!" );
+        System.out.println( "Hello World! This is after my 3rd test!" );
     }
 }
